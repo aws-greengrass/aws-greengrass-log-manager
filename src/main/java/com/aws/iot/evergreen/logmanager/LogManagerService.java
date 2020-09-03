a305ec79c057d5d8328edaa1e5b5ec32dbb980d8
Nikkhil Muthye <nikkhim@amazon.com>
2020-09-03 12:46:20 -0700

    Initial repo setup.

---
+++ src/main/java/com/aws/iot/evergreen/logmanager/LogManagerService.java
@@ -0,0 +1,9 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.aws.iot.evergreen.logmanager;
+
+public class LogManagerService {
+}
